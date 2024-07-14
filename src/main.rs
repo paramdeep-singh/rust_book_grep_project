f80467da7d729423569fd2cc035cca137c431c94
Paramdeep Singh <paramdeep@meta.com>
2024-07-14 14:29:22 -0700

    accepting cmd line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // the convention of including a function's module makes sense for the following line!
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    dbg!(query);
+    dbg!(file_path);
 }
