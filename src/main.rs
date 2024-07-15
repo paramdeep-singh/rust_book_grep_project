4bbb617faa8fb3ff13b433d9f51830b9c73dbf94
Paramdeep Singh <paramdeep@meta.com>
2024-07-14 17:14:59 -0700

    refactored app logic into the run function:

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     // Hmm so assigning owner to a the all important args, storing on the heap initially, and 
     // probably just passing slice references around rest of the code
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file!");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
@@ -19,10 +32,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &Vec<String>) -> Config {
-        Config {
+    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            // this string literal will be stored in the binary, so a reference to it can have the
+            // static lifetime
+            return Err("not enough arguments!");
+        }
+        Ok(Config {
             query: args[1].clone(),
             file_path: args[2].clone(),
-        }
+        })
     }
 }
